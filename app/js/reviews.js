c75c4247cb21fe44ec1422298786b3a0c7cf5420
Pavelbox <pavv.inbox@gmail.com>
2019-08-06 10:49:23 +0300

    brands desktop full

---
+++ app/js/reviews.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	// $('.js-truncate').jTruncate({
+	//     length: 185,
+	//     minTrail: 0,
+	//     moreText: "Читать дальше",
+	//     lessText: "Скрыть",
+	//     moreAni: "fast",
+	//     lessAni: 10
+	// });
+});
