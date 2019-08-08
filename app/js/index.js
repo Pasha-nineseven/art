5b2d609261d38d49079db2a2661dcc15270f532a
pasha-nineseven <pavel@nineseven.ru>
2019-08-08 20:41:14 +0300

    index>600

--- app/js/index.js
+++ app/js/index.js
@@ -79,15 +79,36 @@ $(document).ready(function() {
 			infinite: false,
 			slidesToShow: 6,
   			slidesToScroll: 1,
-  	// 		responsive: [
-			//     {
-			//       breakpoint: 800,
-			//       settings: {
-			//         slidesToShow: 2,
-			//         slidesToScroll: 1,
-			//       }
-			//     },
-			// ]
+  			responsive: [
+			    {
+			      breakpoint: 850,
+			      settings: {
+			        slidesToShow: 5,
+			        slidesToScroll: 1,
+			      }
+			    },
+			    {
+			      breakpoint: 700,
+			      settings: {
+			        slidesToShow: 4,
+			        slidesToScroll: 1,
+			      }
+			    },
+			    {
+			      breakpoint: 600,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 1,
+			      }
+			    },
+			    {
+			      breakpoint: 500,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1,
+			      }
+			    },
+			]
 	    });
 	};
 
